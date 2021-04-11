7173cc2b27e6b95bfc6acc0646d2c987aa064a1a
daeun6 <81478444+daeun6@users.noreply.github.com>
2021-04-11 22:50:11 +0900

    Add files via upload

---
+++ 과제 2-2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int n, i, a;
+
+	cout << "N값을 입력하세요. : ";
+	cin >> n;
+	if (n == 1)
+	{
+		cout << "*";
+	}
+	for (int i = 1; i <= n * 2; i++)//전체 행의 수는 2*n이다
+	{
+		for (int a = 1; a <= n; a++)
+		{
+			if (i % 2 == 0) // 짝수일 경우
+			{
+				if (a % 2 == 0)
+					cout << "*";
+				else cout << " ";
+			}
+			else // 홀수일 경우
+			{
+				if (a % 2 == 0)
+					cout << " ";
+				else cout << "*";
+			}
+		}
+		cout << endl;
+	}
+}
